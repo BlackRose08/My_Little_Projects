3d8830d43eded4b1b39ca323896f8fe87e2a9baa
BlackRose <144360500+BlackRose08@users.noreply.github.com>
2024-12-07 09:22:31 +0100

    Add files via upload

---
+++ C/Cap1/1_1.c
@@ -0,0 +1,14 @@
+/*Scrivere un programma che stampi a video la seguente figura:
+****
+****    */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main(void){
+        
+        printf("****\n");   //Stampa figure
+        printf("****\n");
+    
+        return 0;
+    }
